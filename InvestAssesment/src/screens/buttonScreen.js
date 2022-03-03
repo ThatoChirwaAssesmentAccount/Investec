010b02c9f73071299975bc1660821db83e3b1900
ThatoDeveloper <thato@unitacsolution.co.za>
2022-03-03 18:49:32 +0200

    Create Empty views

---
+++ InvestAssesment/src/screens/buttonScreen.js
@@ -0,0 +1,9 @@
+import React ,{useState} from 'react';
+import { View } from 'react-native';
+export default function ButtonsScreen({ navigation }){
+    return (
+        <View>
+            
+        </View>
+    )
+}
---
+++ InvestAssesment/src/screens/nativeModuleScreen.js
@@ -0,0 +1,9 @@
+import React ,{useState} from 'react';
+import { View } from 'react-native';
+export default function NativeModuleScreen({ navigation }){
+    return (
+        <View>
+            
+        </View>
+    )
+}
